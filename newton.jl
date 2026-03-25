c9774257e0feff60fbf7c47fa4c5bf5260739fe1
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2026-03-25 23:32:06 +0900

    feat: basin grid method

--- newton.jl
+++ newton.jl
@@ -5,8 +5,7 @@ p = Polynomial([-1, 0, 0, 1])
 println("p(z) = ", p)
 println("roots: ", roots(p))
 
-function newton(p, z0; maxiter=100, tol=1e-10)
-    dp = derivative(p)
+function newton(p, dp, z0; maxiter=100, tol=1e-10)
     i = 0
     z = z0
     while i < maxiter
@@ -15,4 +14,11 @@ function newton(p, z0; maxiter=100, tol=1e-10)
         abs(p(z)) < tol && break
     end
     return (z, i)
+end
+
+function basin_grid(p, xrange, yrange, nx, ny; maxiter=100, tol=1e-10)
+    dp = derivative(p)
+    xs = range(xrange[1], xrange[2], length=nx)
+    ys = range(yrange[1], yrange[2], length=ny)
+    [newton(p, dp, x + y*im, maxiter=maxiter, tol=tol) for x in xs, y in ys]
 end
