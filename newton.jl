1133246bb38d0f83ec014dc77dcd9f5ba0a09d82
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2026-03-25 23:24:38 +0900

    feat: newton's method

--- newton.jl
+++ newton.jl
@@ -4,3 +4,15 @@ using Polynomials
 p = Polynomial([-1, 0, 0, 1])
 println("p(z) = ", p)
 println("roots: ", roots(p))
+
+function newton(p, z0; maxiter=100, tol=1e-10)
+    dp = derivative(p)
+    i = 0
+    z = z0
+    while i < maxiter
+        i += 1
+        z = z - p(z) / dp(z)
+        abs(p(z)) < tol && break
+    end
+    return (z, i)
+end
